960680e46d01b1408c5ee3eeef1751fcaba6165c
jasveersinght <jasveersinghtuteja@gmail.com>
2026-02-19 09:47:37 +0530

    UC2:Hardcoded palindrome

---
+++ src/uc2.java
@@ -0,0 +1,17 @@
+public class uc2 {
+    public static void main(String[] args){
+        String input ="madam";
+        boolean isPalindrome = true;
+        for(int i=0;i < input.length() /2;i++){
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+        }
+    }
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+        }
+    }
+}
