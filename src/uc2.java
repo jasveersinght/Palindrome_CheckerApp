3c1cbecd40cafb33c80e369c9dbf03c56866d7b8
jasveersinght <jasveersinghtuteja@gmail.com>
2026-02-19 10:20:19 +0530

    UC6:Hardcoded palindrome

--- src/uc2.java
+++ src/uc2.java
@@ -1,17 +1,27 @@
 public class uc2 {
     public static void main(String[] args){
-        String input ="madam";
+        String input = "civic";
+        int n = input.length();
+        char[] queue = new char[n];
+        int front = 0;
+        int rear = -1;
+        char[] stack = new char[n];
+        int top = -1;
+        for (int i = 0; i < n; i++) {
+            char ch = input.charAt(i);
+            queue[++rear] = ch;
+            stack[++top] = ch;
+        }
         boolean isPalindrome = true;
-        for(int i=0;i < input.length() /2;i++){
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        while (front <= rear) {
+            char fromQueue = queue[front++];
+            char fromStack = stack[top--];
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
+            }
         }
-    }
-        if (isPalindrome) {
-            System.out.println("The string \"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
-        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
